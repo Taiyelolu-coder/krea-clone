84836a20f6f30e448fea8170a06fd285aed39e46
Taiyelolu-coder <alayandeebudola@gmail.com>
2025-09-17 14:32:18 +0100

    fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="flex justify-between items-center px-6 md:px-20">
         <p className="font-semibold text-white text-3xl flex flex-row gap-4 items-center"><img src="/images/krea.jpg" alt="Krea AI Logo" width={50} height={50} className="rounded-lg" />Krea AI</p>
         <p className="text-white font-bold text-2xl flex flex-row gap-4 items-center">
-          curated by <span className="font-semibold text-white text-4xl flex flex-row gap-2"><img src="/images/mobbin.jpg" alt="Mobbin Logo" width={50} height={50} className="rounded-sm" />Mobbin</span>
+          curated by <span className="font-semibold text-white text-4xl flex flex-row gap-2"><img src="/images/Mobbin.jpg" alt="Mobbin Logo" width={50} height={50} className="rounded-sm" />Mobbin</span>
         </p>
       </div>
     </footer>
